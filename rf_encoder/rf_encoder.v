04dded93f95ba2cf325d2ba1696cc7e1518f3ecc
noodlefighter <rongjiayu@gmail.com>
2017-05-14 23:38:16 +0800

    编码模块整合

--- rf_encoder/rf_encoder.v
+++ rf_encoder/rf_encoder.v
@@ -1,8 +1,30 @@
-module rf_encoder (cout, sum, a, b);
-    output cout, sum;
-    input  a, b;
-    wire   cout, sum;
-    
-    assign {cout, sum} = a + b;    
-    
+module rf_encoder (
+    input  rst_n, clk2x, din, enable,
+    output manch_out, miller_out, fmiller_out
+);
+
+manchester_encoder(
+    .rst_n(rst_n),
+    .clk2x(clk2x),
+    .din(din),
+    .enable(enable),
+    .dout(manch_out)
+);
+
+miller_encoder(
+    .rst_n(rst_n),
+    .clk2x(clk2x),
+    .din(din),
+    .enable(enable),
+    .dout(miller_out)    
+);
+
+fmiller_encoder(
+    .rst_n(rst_n),
+    .clk2x(clk2x),
+    .din(din),
+    .enable(enable),
+    .dout(fmiller_out)    
+);
+
 endmodule
